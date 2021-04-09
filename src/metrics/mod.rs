eb71a3d21cf8b1b7c82492aa254d0fc00b9603c0
moz.tools <moz-tools-bot@moz.tools>
2021-04-09 17:24:47 +0000

    Deploy rust-code-analysis

--- src/metrics/mod.rs
+++
@@ -1,8 +0,0 @@
-pub mod cognitive;
-pub mod cyclomatic;
-pub mod exit;
-pub mod fn_args;
-pub mod halstead;
-pub mod loc;
-pub mod mi;
-pub mod nom;
--- src/output/mod.rs
+++
@@ -1,5 +0,0 @@
-pub(crate) mod dump;
-pub use dump::*;
-
-pub(crate) mod dump_metrics;
-pub use dump_metrics::*;
